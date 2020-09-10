9dcda94f4200bdc15d7d53a5468f98ac211a1a5a
Ylan Benoliel <ylanbenoliel@gmail.com>
2020-09-10 12:38:45 -0300

    fix: Update color

--- constants/colors.js
+++ constants/colors.js
@@ -2,7 +2,7 @@ export default {
   mainColor: '#1b3662',
   secondaryColor: '#000D2B',
   navigationColor: '#B87333',
-  whiteColor: '#fff',
+  whiteColor: '#eee',
   accentColor: '#0088a3',
   disableColor: '#444242',
   errorColor: '#f00',
