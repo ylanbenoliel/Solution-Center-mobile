454c0e51c81cea55b1f812e099bae2ae8909275d
Ylan Benoliel <ylanbenoliel@gmail.com>
2020-09-22 17:14:13 -0300

    fix: remove view that cause problems on certains screens

--- components/GeneralStatusBar.js
+++ components/GeneralStatusBar.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import {
-  View, StyleSheet, Platform, StatusBar,
+  StatusBar,
 } from 'react-native';
 
-const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
+// const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
 
 // eslint-disable-next-line react/prop-types
 const GeneralStatusBar = ({ backgroundColor, ...props }) => (
-  <View style={[styles.statusBar, { backgroundColor }]}>
-    <StatusBar translucent backgroundColor={backgroundColor} {...props} />
-  </View>
+  <StatusBar backgroundColor={backgroundColor} {...props} />
 );
 
-const styles = StyleSheet.create({
-  statusBar: {
-    height: STATUSBAR_HEIGHT,
-  },
-});
+// const styles = StyleSheet.create({
+//   statusBar: {
+//     height: STATUSBAR_HEIGHT,
+//   },
+// });
 
 export default GeneralStatusBar;
