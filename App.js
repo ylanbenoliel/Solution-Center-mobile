030e3da970ce117b8a13611e95bcdf2bef06e14f
Ylan Benoliel <ylanbenoliel@gmail.com>
2020-10-19 14:53:43 -0300

    feat: Add new Splash Screen api

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-/* eslint-disable global-require */
-import React, { useEffect, useState } from 'react';
-import { AsyncStorage, Vibration } from 'react-native';
+// /* eslint-disable global-require */
+import React, { useState, useEffect } from 'react';
+import {
+  AsyncStorage, Vibration,
+} from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { useFonts } from '@use-expo/font';
-import { AppLoading, Notifications } from 'expo';
+import { Notifications } from 'expo';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { AuthProvider } from './contexts/auth';
 import Route from './routes/Route';
@@ -12,12 +15,31 @@ import { api } from './services/api';
 
 export default function App() {
   const [admin, setAdmin] = useState('');
-
+  const [appReady, setAppReady] = useState(false);
+  const [loaded] = useFonts({
+    // eslint-disable-next-line global-require
+    'Amaranth-Regular': require('./assets/fonts/Amaranth-Regular.ttf'),
+  });
   useEffect(() => {
+    handleInitApp();
+  }, []);
+
+  async function handleInitApp() {
+    try {
+      await SplashScreen.preventAutoHideAsync();
+    } catch (error) {
+      await SplashScreen.hideAsync();
+    }
+    prepareResources();
+  }
+
+  async function prepareResources() {
     getToken();
     getPrivilegies();
     Notifications.addListener(handleNotification);
-  }, []);
+    setAppReady(true);
+    await SplashScreen.hideAsync();
+  }
 
   async function getToken() {
     api.interceptors.request.use(async (config) => {
@@ -42,16 +64,13 @@ export default function App() {
       setAdmin(null);
     }
   }
+
   async function handleNotification() {
     Vibration.vibrate();
   }
 
-  const [fontsLoaded] = useFonts({
-    'Amaranth-Regular': require('./assets/fonts/Amaranth-Regular.ttf'),
-  });
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
+  if (!loaded || !appReady) {
+    return null;
   }
 
   return (
