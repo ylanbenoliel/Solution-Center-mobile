e74f61930f820884c02f1af0a3e78e9ba2f41c1d
Ylan Benoliel <ylanbenoliel@gmail.com>
2021-02-07 20:25:55 -0300

    fix: Fetch correct page on end of list

--- components/UserEventsModal.js
+++ components/UserEventsModal.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React, { useState, memo } from 'react';
 import {
   View, Text, StyleSheet, TouchableOpacity, FlatList, Alert,
 } from 'react-native';
@@ -12,26 +12,19 @@ import { Feather } from '@expo/vector-icons';
 import ListEmpty from '@components/ListEmpty';
 import Separator from '@components/Separator';
 
+import { removeDuplicates, roomById } from '@helpers/functions';
+
 import { api } from '@services/api';
 
 import colors from '@constants/colors';
-import { ROOM_DATA } from '@constants/fixedValues';
 
 const EventInfo = ({
   date, time, room, status_payment: status,
 }) => {
-  const roomName = ROOM_DATA.map((data) => {
-    if (data.room === Number(room)) { return data.name.split(' ')[0]; }
-    return false;
-  });
-
+  const roomName = roomById(room);
   return (
     <View style={styles.eventInfoContainer}>
-      <View style={{
-        flex: 1,
-      }}
-      >
-
+      <View style={{ flex: 1 }}>
         <Text style={styles.text}>
           Dia:
           {' '}
@@ -52,22 +45,14 @@ const EventInfo = ({
         </Text>
       </View>
 
-      <View style={{
-        flex: 1, height: '100%', alignItems: 'center',
-      }}
-      >
+      <View style={{ flex: 1, height: '100%', alignItems: 'center' }}>
         <Text style={styles.text}>Pagamento</Text>
         {Number(status) === 0 ? (
           <TouchableOpacity onPress={() => {
             Alert.alert('', 'Pagamento em aberto',
-              [
-                {
-                  text: 'Ok',
-                },
-              ]);
+              [{ text: 'Ok' }]);
           }}
           >
-
             <Feather
               name="x"
               size={scale(24)}
@@ -87,55 +72,44 @@ const EventInfo = ({
   );
 };
 
+const EventInfoMemo = memo(EventInfo);
+
 const UserEventsModal = ({ isVisible, onClose }) => {
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(null);
-  const [label, setLabel] = useState('Carregando...');
-  const [events, setEvents] = useState([]);
+  const [events, setEvents] = useState(null);
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     getData();
-  //     setRefreshList(!refreshList);
-  //   }, 2000);
-  // }, [!!isVisible]);
-
-  // useEffect(() => {
-  //   getData();
-  // }, [page]);
-
-  function getData() {
-    if (totalPages && (page > totalPages)) {
-      return null;
-    }
-    api.get(`/events/list/user?page=${page}`)
-      .then((res) => {
-        const incomingEvents = (res.data.data);
-        if (page === 1) {
-          if (incomingEvents.length === 0) {
-            return setLabel('Sem reservas.');
-          }
-          return setEvents(incomingEvents);
+  async function fetchData(pageToLoad = 1) {
+    try {
+      if (totalPages && (pageToLoad > totalPages)) {
+        return;
+      }
+      const eventsResponse = await api.get(`/events/list/user?page=${pageToLoad}`);
+      const incomingEvents = (eventsResponse.data.data);
+      if (pageToLoad === 1) {
+        if (incomingEvents.length === 0) {
+          setEvents([]);
+          return;
         }
-        const combineEvents = [...events, ...incomingEvents];
-        setTotalPages(res.data.lastPage);
-        return setEvents(combineEvents);
-      })
-      .catch(() => setLabel('Erro ao pesquisar reservas.'));
-    return null;
-  }
-
-  function handleLoadMore() {
-    setPage(page + 1);
-    getData();
+        setEvents(incomingEvents);
+        return;
+      }
+      const combineEvents = [...events, ...incomingEvents];
+      const eventsWithoutDuplicates = removeDuplicates(combineEvents, 'id');
+      setTotalPages(eventsResponse.data.lastPage);
+      setEvents(eventsWithoutDuplicates);
+      setPage(page + 1);
+    } catch (error) {
+      setEvents([]);
+    }
   }
 
   function handleOpenModal() {
-    getData();
+    fetchData();
   }
 
   function handleCloseModal() {
-    setEvents([]);
+    setEvents(null);
     setPage(1);
   }
 
@@ -166,19 +140,22 @@ const UserEventsModal = ({ isVisible, onClose }) => {
       </View>
 
       <View style={{ flex: 9, width: '90%' }}>
-
         <FlatList
           data={events}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item: event }) => (
-            <EventInfo key={event.id} {...event} />
+            <EventInfoMemo key={event.id} {...event} />
           )}
           ItemSeparatorComponent={() => (<Separator />)}
-          onEndReachedThreshold={0.5}
-          onEndReached={() => handleLoadMore()}
-          ListEmptyComponent={<ListEmpty modal label={label} />}
+          onEndReachedThreshold={0.6}
+          onEndReached={() => fetchData(page + 1)}
+          ListEmptyComponent={(
+            <ListEmpty
+              modal
+              label={events ? 'Sem mensagens.' : 'Carregando...'}
+            />
+          )}
         />
-
       </View>
 
     </Modal>
