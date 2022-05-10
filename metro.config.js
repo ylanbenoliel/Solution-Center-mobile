7dacfcb572d79802fb2e30687ba204181ac7ce65
Ylan Benoliel <ylanbenoliel@gmail.com>
2022-05-10 07:36:14 -0300

    feat: Update metro config

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,11 @@
-const { getDefaultConfig } = require('metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
-    resolver: {
-      assetExts: assetExts.filter((ext) => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-    },
-  };
-})();
+const config = getDefaultConfig(__dirname);
+
+config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
+
+const assetExts = config.resolver.assetExts.filter((ext) => ext !== 'svg');
+config.resolver.assetExts = assetExts;
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
+
+module.exports = config;
